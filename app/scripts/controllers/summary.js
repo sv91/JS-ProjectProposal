47994103ac8780b8f39c4d97ddf26623c3643b05
Alexander Vostriakov <alexander.vostriakov@epfl.ch>
2016-04-22 02:17:22 +0200

    Pré-deploy

--- app/scripts/controllers/summary.js
+++ app/scripts/controllers/summary.js
@@ -20,40 +20,48 @@ angular.module('projectProposalApp')
 
   function addIfNotNull(val){
     if (val!=null && val!=undefined ){
-    return val;
+      return val;
     }
     return 0;
   }
 
   function refresh(){
-  angular.forEach($scope.record.deliverables,function(val){
-    if ($scope.summ.softdev.indexOf(val.softdev)==-1){
-        $scope.summ.softdev.push(val.softdev);
-    }
-    if ($scope.summ.collabs.indexOf(val.collabs)==-1){
-        $scope.summ.collabs.push(val.collabs);
-    }
-    if ($scope.summ.architecture.indexOf(val.architecture)==-1){
-        $scope.summ.architecture.push(val.architecture);
-    }
-    $scope.summ.hpc.runs+= addIfNotNull(val.HPC.runs);
-    $scope.summ.hpc.partition+= addIfNotNull(val.HPC.partition);
-    $scope.summ.hpc.time+= addIfNotNull(val.HPC.time);
-    $scope.summ.hpc.numarte+= addIfNotNull(val.HPC.numarte);
-    $scope.summ.hpc.sizearte+= addIfNotNull(val.HPC.sizearte)*addIfNotNull(val.HPC.numarte);
-    $scope.summ.cloud.runs+= addIfNotNull(val.cloud.runs);
-    $scope.summ.cloud.partition+= addIfNotNull(val.cloud.partition);
-    $scope.summ.cloud.time+= addIfNotNull(val.cloud.time);
-    $scope.summ.cloud.numarte+= addIfNotNull(val.cloud.numarte);
-    $scope.summ.cloud.sizearte+= addIfNotNull(val.cloud.sizearte)*addIfNotNull(val.cloud.numarte);
+    angular.forEach($scope.record.deliverables,function(val){
+      angular.forEach(val.softdev,function(val2){
+        if ($scope.summ.softdev.indexOf(val2)==-1){
+          $scope.summ.softdev.push(val2);
+        }
+      })
+      angular.forEach(val.collabs,function(val2){
+        if ($scope.summ.collabs.indexOf(val2)==-1){
+          $scope.summ.collabs.push(val2);
+        }
+      })
+      angular.forEach(val.architecture,function(val2){
+        if ($scope.summ.architecture.indexOf(val2)==-1){
+          $scope.summ.architecture.push(val2);
+        }
+      })
+      $scope.summ.hpc.runs+= addIfNotNull(val.HPC.runs);
+      $scope.summ.hpc.partition+= addIfNotNull(val.HPC.partition);
+      $scope.summ.hpc.time+= addIfNotNull(val.HPC.time);
+      $scope.summ.hpc.numarte+= addIfNotNull(val.HPC.numarte);
+      $scope.summ.hpc.sizearte+= addIfNotNull(val.HPC.sizearte)*addIfNotNull(val.HPC.numarte);
+      $scope.summ.cloud.runs+= addIfNotNull(val.cloud.runs);
+      $scope.summ.cloud.partition+= addIfNotNull(val.cloud.partition);
+      $scope.summ.cloud.time+= addIfNotNull(val.cloud.time);
+      $scope.summ.cloud.numarte+= addIfNotNull(val.cloud.numarte);
+      $scope.summ.cloud.sizearte+= addIfNotNull(val.cloud.sizearte)*addIfNotNull(val.cloud.numarte);
 
-    if ($scope.summ.hardware.indexOf(val.hardware)==-1){
-        $scope.summ.hardware.push(val.hardware);
-    }
-  });
+      angular.forEach(val.hardware,function(val2){
+        if ($scope.summ.hardware.indexOf(val2)==-1){
+          $scope.summ.hardware.push(val2);
+        }
+      })
+    });
 
-  $scope.summ.cloud.sizearte = $scope.summ.cloud.sizearte/$scope.summ.cloud.numarte;
-  $scope.summ.hpc.sizearte = $scope.summ.hpc.sizearte/$scope.summ.hpc.numarte;
-}
-refresh();
+    $scope.summ.cloud.sizearte = $scope.summ.cloud.sizearte/$scope.summ.cloud.numarte;
+    $scope.summ.hpc.sizearte = $scope.summ.hpc.sizearte/$scope.summ.hpc.numarte;
+  }
+  refresh();
 });
