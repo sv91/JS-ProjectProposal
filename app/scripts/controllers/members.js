fc456a0db203f7aa95c9a3269879ac690d4fe0d1
Alexander Vostriakov <alexander.vostriakov@epfl.ch>
2016-04-22 13:45:35 +0200

    Members

--- app/scripts/controllers/members.js
+++ app/scripts/controllers/members.js
@@ -9,35 +9,27 @@
  */
 angular.module('projectProposalApp')
   .controller('MembersCtrl', function ($scope) {
-    $scope.record = {
-      "principalInvestigator": {},
-      "coPrincipalInvestigator":{},
-  /*    "memberList":[
-        "name":{},
-        "sciper":{}
-      ]*/
-    };
     })
-    .directive('singleUserSelect', function($log, hbpIdentityUserDirectory) {
+    .directive('singleUserSelectPi', function($log, hbpIdentityUserDirectory) {
     return {
     	scope: true,
-    template: '<hbp-user-selector hbp-on-select="handleUserSelection"></hbp-user-selector><hbp-usercard hbp-user="selectedUser" ></hbp-usercard></pre>',
+    template: '<hbp-user-selector hbp-on-select="handleUserSelection(user)"></hbp-user-selector><hbp-usercard hbp-user="record.pi" ></hbp-usercard></pre>',
     link: function(scope, elt, attr) {
-    	// check if a user id is already set
-    	var userId = elt.parent().find('input').attr('value');
-    	if (userId) {
-    		hbpIdentityUserDirectory.get([userId])
-    		.then(function(users) {
-    			scope.selectedUser = users[userId];
-    		})
-    		.catch($log.error);
-    	}
-
-    	scope.selectedUser = null;
     	scope.handleUserSelection = function(options) {
     			console.log('select', arguments);
-    			scope.selectedUser = options.user;
-    			elt.parent().find('input').attr('value', scope.selectedUser.id);
+    			scope.record.pi = options;
+    	}
+    }
+    }
+    })
+    .directive('singleUserSelectCopi', function($log, hbpIdentityUserDirectory) {
+    return {
+    	scope: true,
+    template: '<hbp-user-selector hbp-on-select="handleUserSelection(user)"></hbp-user-selector><hbp-usercard hbp-user="record.copi" ></hbp-usercard></pre>',
+    link: function(scope, elt, attr) {
+    	scope.handleUserSelection = function(options) {
+    			console.log('select', arguments);
+    			scope.record.copi = options;
     	}
     }
     }
@@ -46,13 +38,14 @@ angular.module('projectProposalApp')
     .directive('multiUserSelect', function() {
     	return {
     		scope: true,
-    		template: '<hbp-user-selector hbp-on-select="handleUserSelection"></hbp-user-selector><hbp-usercard ng-repeat="u in selectedUsers" hbp-user="u" ></hbp-usercard></pre>',
+    		template: '<hbp-user-selector hbp-on-select="handleUserSelection(user)"></hbp-user-selector><hbp-usercard ng-repeat="u in record.members" hbp-user="u" ></hbp-usercard></pre>',
     		link: function(scope, elt, attr) {
-    			scope.selectedUsers = [];
     			scope.handleUserSelection = function(options) {
+            if(scope.record.members==undefined ||scope.record.members==null){
+              scope.record.members = [];
+            }
     				console.log('select', arguments);
-    				scope.selectedUsers.push(options.user);
-    				elt.parent().find('input').attr(value, scope.selectedUser);
+    				scope.record.members.push(options);
     			}
     		}
     	}
