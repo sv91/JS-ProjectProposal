7768bbddf2eb7678d1e0ccd4834abd82e5d519dc
Alexander Vostriakov <alexander.vostriakov@epfl.ch>
2016-05-02 11:51:35 +0200

    Test jsPDF

---
+++ app/scripts/controllers/finalize.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+* @ngdoc function
+* @name projectProposalApp.controller:FinalizeCtrl
+* @description
+* # FinalizeCtrl
+* Controller responsible for the finalize page.
+*/
+angular.module('projectProposalApp')
+.controller('FinalizeCtrl', function ($scope) {
+  function generatePDF($scope){
+    var doc = new jsPDF();
+  }
+});
